0c406151cca71d64726f07e3207512d180d90d08
Tigran Iskandaryan <tiskandaryan@griddynamics.com>
2022-11-26 13:57:40 +0300

    Fixed the indentation

--- Example/Tests/TestSr25519.swift
+++ Example/Tests/TestSr25519.swift
@@ -3,7 +3,8 @@ import EncryptingSwift
 import CommonSwift
 
 class TestSr25519: XCTestCase {
-    private let testValues = (0..<Constants.testsCount).compactMap { _ in UUID().uuidString.data(using: .utf8)?[0..<32] }
+    private let testValues = (0..<Constants.testsCount)
+        .compactMap { _ in UUID().uuidString.data(using: .utf8)?[0..<32] }
     
     func testSr25519() throws {
         guard let seed = "0x7032fc2571822aaaf374cc5deb44e92d7ff97c314bea704aab95c04c4c4229b1".hex.decode() else {
@@ -14,7 +15,11 @@ class TestSr25519: XCTestCase {
         for testValue in testValues {
             let keyPair = try testValue.sr25519.keyPair(seed: seed)
            
-            let isValid = try testValue.sr25519.verify(signature: keyPair.signature, publicKey: keyPair.publicKey)
+            let isValid = try testValue.sr25519.verify(
+                signature: keyPair.signature,
+                publicKey: keyPair.publicKey
+            )
+            
             XCTAssertEqual(isValid, true)
         }
     }
